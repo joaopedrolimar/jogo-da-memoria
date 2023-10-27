92d46f963965b6c706326d7119433d7ae013435a
joao pedro <joaopedrolimar@gmail.com>
2023-10-27 16:53:57 -0300

    :tada: Create card commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame( ) {
+    return`
+        <article class ="card-game">
+            <img src= "./src/components/img/alura-pixel.png" >
+        <article/>
+    `
+}
+
+export default CardGame;
